bded76c64620c677aa6e1ca554c92433bd35cc4f
EBNoir <100785117+EBNoir@users.noreply.github.com>
2022-11-11 01:25:17 -0300

    Update App.js

--- resumenes/src/App.js
+++ resumenes/src/App.js
@@ -5,12 +5,33 @@ import Materias from './components/materias';
 import Registro from './components/registro';
 import {useQuery, gql} from '@apollo/client';
 
+const GET_USUARIOS = gql`
+query getUsuarios{
+  getUsuarios{
+    id 
+    email
+    pass
+  }
+}`;
+
+function MostrarUsuarios() {
+  const {loading, error, data} = useQuery(GET_USUARIOS);
+  if(loading) return (<p>cargando...</p>);
+  if(error) return (<p>error</p>);
+  return(
+    <ul>
+      data.getUsuarios.map(obj => {
+        <li key={obj.id}>{obj.email}</li>
+      })
+    </ul>)
+}
 
 function App() {
   return (
     
     <div>
       <h1>Resumenes</h1>
+      <MostrarUsuarios></MostrarUsuarios>
       <Materias/>
     </div>
     
