88663387f4e693a1c8c1f0ba778bfbc29a017a23
EBNoir <100785117+EBNoir@users.noreply.github.com>
2022-11-10 23:07:24 -0300

    Update index.js

--- resumenes/src/index.js
+++ resumenes/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import{ApolloClient, InMemoryCache, ApolloProvider,gql} from'@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8090/graphql',
+  cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
