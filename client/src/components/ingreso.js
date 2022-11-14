ed426d8366dcf18d0cdd0393f70c4b1b32692713
Aldo Muñoz Arellano <aldomunozarellano@Aldos-MacBook-Pro.local>
2022-11-14 16:25:45 -0300

    className ingreso.js

--- client/src/components/ingreso.js
+++ client/src/components/ingreso.js
@@ -11,29 +11,29 @@ export class Ingreso extends Component {
               </a>
             </header>  
           </div>
-<section class="vh-100 gradient-custom" className='loginSection'>
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-6">
-        <div class="card bg-dark text-white" style={{borderRadius: "1rem"}}>
-          <div class="card-body p-5 text-center">
+<section className='vh-100 gradient-custom loginSection'>
+  <div className="container py-5 h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-12 col-md-8 col-lg-6 col-xl-6">
+        <div className="card bg-dark text-white" style={{borderRadius: "1rem"}}>
+          <div className="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+            <div className="mb-md-5 mt-md-4 pb-5">
 
-              <h2 class="fw-bold mb-2 text-uppercase">Bienvenid@ a Resúmenes UAI</h2><br/>
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" placeholder='Usuario' class="form-control form-control-lg" />
+              <h2 className="fw-bold mb-2 text-uppercase">Bienvenid@ a Resúmenes UAI</h2><br/>
+              <div className="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" placeholder='Usuario' className="form-control form-control-lg" />
               </div>
 
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" placeholder='Contraseña' class="form-control form-control-lg" />
+              <div className="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" placeholder='Contraseña' className="form-control form-control-lg" />
               </div>
 
-              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">¿Olvidaste tu contraseña?</a></p>
-                <div class="container">
-                  <a href="#" class="btn btn-primary btn-lg mx-3">Iniciar Sesión</a>
+              <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">¿Olvidaste tu contraseña?</a></p>
+                <div className="container">
+                  <a href="#" className="btn btn-primary btn-lg mx-3">Iniciar Sesión</a>
 
-                  <a href="#" class="btn bg-white fg-dark btn-lg mx-3">Registrarse</a>
+                  <a href="#" className="btn bg-white fg-dark btn-lg mx-3">Registrarse</a>
                 </div> 
 
             </div>
