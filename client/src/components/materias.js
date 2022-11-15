659f5741b94bd9423fd25340ca17a68556ace5d3
morozco0 <morozco@alumnos.uai.cl>
2022-11-14 23:13:32 -0300

    Update materias.js

--- client/src/components/materias.js
+++ client/src/components/materias.js
@@ -42,21 +42,21 @@ export default class Materias extends Component{
                                     <Card.Img variant="top" src={"https://edem.eu/wp-content/uploads/2021/10/calculo-diferencial.jpg"} />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>CÁLCULO DIFERENCIAL</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>ÁLGEBRA</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>PROGRAMACIÓN</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                             </CardGroup>
@@ -70,28 +70,28 @@ export default class Materias extends Component{
                                     <Card.Img variant="top" src={"https://edem.eu/wp-content/uploads/2021/10/calculo-diferencial.jpg"} />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>CÁLCULO INTEGRAL</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>ÁLGEBRA LINEAL</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>FÍSICA I</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>TALLER DE INTRODUCCIÓN A LA INGENIERÍA</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                             </CardGroup>
@@ -105,28 +105,28 @@ export default class Materias extends Component{
                                     <Card.Img variant="top" src={"https://edem.eu/wp-content/uploads/2021/10/calculo-diferencial.jpg"} />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>CÁLCULO MULTIVARIABLES</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>PROBABILIDAD Y ESTADÍSTICA</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>FÍSICA II</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>FUNDAMENTOS DE CIENCIAS AMBIENTALES</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                             </CardGroup>
@@ -140,28 +140,28 @@ export default class Materias extends Component{
                                     <Card.Img variant="top" src={"https://edem.eu/wp-content/uploads/2021/10/calculo-diferencial.jpg"} />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>ECUACIONES DIFERENCIALES</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>FÍSICA III</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>FUNDAMENTOS DE CIENCIAS DE DATOS</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                                 <Card>
                                     <Card.Img variant="top" src="holder.js/100px160" />
                                     <Card.Body>
                                     <Card.Title style={{ height: '3.5rem'}}>TALLER DE DISEÑO EN INGENIERÍA</Card.Title>
-                                    <Button variant="primary">Go somewhere</Button>
+                                    <Button variant="primary">Ver Resúmenes</Button>
                                     </Card.Body>
                                 </Card>
                             </CardGroup>
@@ -181,21 +181,21 @@ export default class Materias extends Component{
                                         <Card.Img variant="top" src={""} />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>MINERÍA DE DATOS</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem' }}>LENGUAJES Y PARADIGMAS DE PROGRAMACIÓN</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem' }}>ESTRUCTURA DE DATOS Y ALGORITMOS</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                 </CardGroup>
@@ -208,21 +208,21 @@ export default class Materias extends Component{
                                         <Card.Img variant="top" src={""} />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>BASES DE DATOS</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>INTELIGENCIA ARTIFICIAL</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>DISEÑO DE SOFTWARE</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                 </CardGroup>
@@ -242,21 +242,21 @@ export default class Materias extends Component{
                                         <Card.Img variant="top" src={""} />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>CIVILIZACIÓN CONTEMPORÁNEA</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>ESCRITURA ARGUMENTATIVA</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>LITERATURA Y HUMANIDADES</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                 </CardGroup>
@@ -269,21 +269,21 @@ export default class Materias extends Component{
                                         <Card.Img variant="top" src={""} />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>CIENCIAS</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>ÉTICA</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                     <Card>
                                         <Card.Img variant="top" src="" />
                                         <Card.Body>
                                         <Card.Title style={{ height: '3.5rem'}}>ARTE Y HUMANIDADES</Card.Title>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Button variant="primary">Ver Resúmenes</Button>
                                         </Card.Body>
                                     </Card>
                                 </CardGroup>
